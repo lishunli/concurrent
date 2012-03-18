3077df95c47421ae5fd47415b283b8a49fa263aa
lishunli <leeshunli@qq.com>
2012-03-18 22:54:12 +0800

    update some future demo.

--- src/main/java/org/usc/concurrent/CallableAndFuture.java
+++ src/main/java/org/usc/concurrent/CallableAndFuture.java
@@ -14,7 +14,7 @@ public class CallableAndFuture {
     public static void main(String[] args) {
         ExecutorService threadPool = Executors.newSingleThreadExecutor();
         // Callable要采用ExecutorSevice的submit方法提交，返回的future对象可以取消任务
-        Future<String> future = threadPool.submit(
+        final Future<String> future = threadPool.submit(
                 new Callable<String>() {
                     public String call() throws Exception {
                         Thread.sleep(2000);
@@ -23,14 +23,30 @@ public class CallableAndFuture {
                 }
                 );
         // Future取得的结果类型和Callable返回的结果类型必须一致
-        System.out.println("等待结果");
-        try {
-            System.out.println("拿到结果：" + future.get());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+        // System.out.println("等待结果1");
+        // try {
+        // System.out.println("拿到结果1：" + future.get());
+        // } catch (InterruptedException e) {
+        // e.printStackTrace();
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+
+        new Thread(new Runnable() {
+            public void run() {
+                System.out.println("等待结果2");
+                try {
+                    System.out.println("拿到结果2：" + future.get());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        System.out.println("end..");
 
         ExecutorService threadPool2 = Executors.newFixedThreadPool(10);
         // CompletionService用于提交一组Callable任务，其take方法返回已完成的一个Callable任务对应的Future对象
