9bc5d7cd49b135ba2ed5b05869483d0d8a3d500c
lishunli <leeshunli@qq.com>
2012-03-18 17:30:51 +0800

    add comment.

--- src/main/java/org/usc/concurrent/ThreadPoolTest.java
+++ src/main/java/org/usc/concurrent/ThreadPoolTest.java
@@ -5,15 +5,16 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 线程池
+ * 线程池的概念与Executors类的应用
  *
  * @author ShunLi
  */
 public class ThreadPoolTest {
     public static void main(String[] args) {
-        ExecutorService threadPool = Executors.newFixedThreadPool(3);
-        // ExecutorService threadPool = Executors.newCachedThreadPool();
-        // ExecutorService threadPool = Executors.newSingleThreadExecutor();
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);// 创建固定大小的线程池
+        // ExecutorService threadPool = Executors.newCachedThreadPool();// 创建缓存线程池
+        // ExecutorService threadPool = Executors.newSingleThreadExecutor();// 创建单一线程池
         // SingleThreadExecutor比普通的单线程好处在于，如果一个线程死了，SingleThreadExecutor 照样会创建另一个单独的线程
 
         for (int i = 1; i <= 10; i++) {
@@ -35,6 +36,7 @@ public class ThreadPoolTest {
         System.out.println("all of 10 tasks have committed! ");
         threadPool.shutdown(); // Runnable 全部做完后就结束线程池
 
+        // 用线程池启动定时器
         Executors.newScheduledThreadPool(3).scheduleAtFixedRate(
                 new Runnable() {
                     public void run() {
